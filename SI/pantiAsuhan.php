887507d003bd731b5f401114215bd56614416866
razes <razes.rma@gmail.com>
2018-04-30 13:55:35 +0700

    last update
    maintnance database

---
+++ SI/pantiAsuhan.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+<title>Sistem Informasi Panti Asuhan dan Panti Jompo</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+
+<div class="wrapper row0">
+  <div id="topbar" class="hoc clear"> 
+    <div class="fl_left">
+      <ul class="nospace">
+        <li><a href="index.php"><i class="fa fa-lg fa-home"></i></a></li>
+        <li class="active"><a href="http://localhost/SI/pantiAsuhan.php" style="color: red;">PANTI ASUHAN</a></li>
+        <li class="active"><a href="http://localhost/SI/pantiJompo.php">PANTI JOMPO</li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="nospace">
+        <li><i class="fa fa-phone"></i> (0341) 412266</li>
+        <li><i class="fa fa-envelope-o"></i> dinsos@malangkota.go.id</li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <div id="logo" class="fl_left">
+      <h1><a href="#"></a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li><a href="http://localhost/SI/pages/pantiAsuhan/berita.php">BERITA</a></li>
+        <li><a href="http://localhost/SI/pages/pantiAsuhan/daftarPanti.php">DAFTAR PANTI</a></li>
+        <li><a href="http://localhost/SI/pages/pantiAsuhan/galeri.php">GALERI</a></li>
+        <li><a href="http://localhost/SI/pages/pantiAsuhan/adopsi.php">ADOPSI</a></li>
+        <li><a href="login.php">LOGIN</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+
+<div class="wrapper bgded overlay" style="height: 150px;">
+  <div id="pageintro" class="hoc clear"> 
+  </div>
+</div>
+
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+       <section id="introblocks">
+      <div class="sectiontitle">
+        <h6 class="heading">Panti Asuhan Kota Malang</h6>
+        <p>--------------------------------------------------</p>
+      </div>
+      <ul class="nospace btmspace-80 group">
+        <li class="one_third first">
+          <article><i class="fa fa-diamond"></i>
+            <h6 class="heading font-x1"><a href="">DAFTAR PANTI</a></h6>
+      <p> Daftar panti asuhan di Kota Malang </p>
+          </article>
+        </li>
+        <li class="one_third">
+          <article><i class="fa fa-fort-awesome"></i>
+            <h6 class="heading font-x1"><a href="">GALERI</a></h6>
+      <p> Sekumpulan memori anak panti yang hangat </p>
+          </article>
+        </li>
+        <li class="one_third">
+          <article><i class="fa fa-forumbee"></i>
+            <h6 class="heading font-x1"><a href="#">BERITA</a></h6>
+      <p> Berita kegiatan yang di Panti Jompo</p>
+          </article>
+        </li>
+      </ul>
+      <p class="center"><a class="btn" href="#">DONASI</a></p>
+    </section>
+    <!-- ################################################################################################ -->
+  </main>
+</div>
+
+
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
